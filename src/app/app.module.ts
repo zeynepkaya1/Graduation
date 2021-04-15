45420d6628ea5a7b31921b9dde507ae2c7fd6015
Zeynep Kaya <1998.ze.ka@gmail.com>
2021-04-16 01:21:52 +0300

    most visited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { CoverComponent } from './cover/cover.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoverComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,

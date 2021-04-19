a7426b244086343d8ab3290dbbd48eb845f7dfae
Zeynep Kaya <1998.ze.ka@gmail.com>
2021-04-19 19:42:32 +0300

    location table

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { LocationModel } from './location.model';
-import { Location} from '../location';
+
 
 @Component({
   selector: 'location-component',
@@ -10,29 +9,9 @@ import { Location} from '../location';
 })
 export class LocationComponent{
 
-  // locModel= new LocationModel();
+  locModel= new LocationModel();
 
-  Locations: Location[] = [];
-
-  //locModel: LocationModel[] = [];
-
-  constructor( private httpClient: HttpClient){  }
-
-
-  ngOnInit() {
-
-    //this.locModel = []
-
-    let resp =this.httpClient.get('../../assets/Locations.json');
-    resp.subscribe((data) => this.Locations);
-
-    // this.httpClient.get('../../assets/Locations.json').subscribe((result: Location[]) => {
-    //   this.Locations = result;
-    // });
-
+  getItems(){
+    return this.locModel.locations;
   }
-
-  // getItems(){
-  //   return this.locModel.locations;
-  // }
 }
--- src/app/location/location.model.ts
+++ src/app/location/location.model.ts
@@ -1,25 +1,22 @@
 export class LocationModel {
 
-  City!: string;
-  Country!: string;
+  locations: any;
 
-//   locations: any;
+  constructor() {
+    this.locations = [
+      new locItems("Paris", "France"),
+      new locItems("London", "England"),
+      new locItems("New york", "USA")
+    ];
+  }
+}
 
-//   constructor() {
-//     this.locations = [
-//       new locItems("Paris", "France"),
-//       new locItems("London", "England"),
-//       new locItems("New york", "USA")
-//     ];
-//   }
-// }
+export class locItems {
+  city;
+  country;
 
-// export class locItems {
-//   city;
-//   country;
-
-//   constructor(city: string, country: string){
-//     this.city = city;
-//     this.country = country;
-//   }
+  constructor(city: string, country: string){
+    this.city = city;
+    this.country = country;
+  }
 }

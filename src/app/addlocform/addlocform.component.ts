20a6e9f13dd404f6c94b5c5adf9d5809aca36724
Zeynep Kaya <1998.ze.ka@gmail.com>
2021-06-05 13:24:04 +0300

    adloc-done

--- src/app/addlocform/addlocform.component.ts
+++ src/app/addlocform/addlocform.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { postLoc } from '../profile/post.model';
 
+
+
+
 @Component({
   selector: 'app-addlocform',
   templateUrl: './addlocform.component.html',
@@ -11,19 +14,38 @@ export class AddlocformComponent implements OnInit {
   postLoc = new postLoc();
   dataarray=[];
 
+  city: any;
+  country:any;
+  winter:any;
+  water:any;
+  culture:any;
+  nature:any;
+  entertainment:any;
+  camping:any;
+  cruise:any;
+  obj:any;
   constructor() { }
 
   ngOnInit(){
     this.postLoc= new postLoc;
-    this.dataarray.push(this.postLoc);
   }
 
   addForm(){
     this.postLoc= new postLoc;
-    this.dataarray.push(this.postLoc);
   }
 
   onsubmit(){
-    console.log(this.dataarray);
+    this.obj={
+  city:this.city,
+  country:this.country,
+  winter:this.winter,
+  water:this.water,
+  culture:this.culture,
+  nature:this.nature,
+  entertainment:this.entertainment,
+  camping:this.culture,
+  cruise:this.cruise
+    }
+    console.log(this.obj)
   }
 }

d7eeb604bc722af3b39f845e0d2b77ad19fc9b5f
Zeynep Kaya <1998.ze.ka@gmail.com>
2021-06-22 16:31:24 +0300

    addloc-bir

--- src/app/location/location.model.ts
+++ src/app/location/location.model.ts
@@ -6,7 +6,23 @@ export class LocationModel {
     this.locations = [
       new locItems("Paris", "France"),
       new locItems("London", "England"),
-      new locItems("New york", "USA")
+      new locItems("Hong kong", "China"),
+      new locItems("Istanbul", "Turkey"),
+      new locItems("Bangkok", "Thailand"),
+      new locItems("Singapure", "Singapure"),
+      new locItems("Dubai", "United Arab Emirates"),
+      new locItems("Antalya", "Turkey"),
+      new locItems("Delhi", "India"),
+      new locItems("Shenzhen", "China"),
+      new locItems("Phuket", "Thailand"),
+      new locItems("Rome", "Italy"),
+      new locItems("Tokyo", "Japan"),
+      new locItems("Prague", "Czech Republic"),
+      new locItems("Seoul", "South Korea"),
+      new locItems("Amsterdam", "Netherlands"),
+      new locItems("Miami", "United States"),
+      new locItems("Osaka", "Japan"),
+      new locItems("Las Vegas", "United States"),
     ];
   }
 }
--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -14,42 +14,74 @@ const material = [
   templateUrl: './profile.html',
   styleUrls: ['./profile.css']
 })
-export class ProfileComponent{
+export class ProfileComponent {
 
   data: any;
-  servicedata:any;
+  servicedata: any;
   postLoc = new postLoc();
-  postLocArray=[];
-  activeindex=-1;
-  title='Submit';
+  postLocArray = [];
+  activeindex = -1;
+  title = 'Submit';
 
-  onSubmit(){
-    if(this.activeindex == 1){
+  city= '';
+  country= '';
+  winter= '';
+  water= '';
+  culture= '';
+  nature= '';
+  entertainment= '';
+  camping= '';
+  cruise= '';
+  obj= '';
 
-    }
+
+  City= '';
+  Country= '';
+  Winter= '';
+  Water= '';
+  Culture= '';
+  Nature= '';
+  Entertainment= '';
+  Camping= '';
+  Cruise= '';
+  Obj= '';
+
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    this.postLoc = new postLoc;
   }
 
-  // postLoc = new postLoc();
+  addForm() {
+    this.postLoc = new postLoc;
+  }
 
-  // dataarray = [];
+  onsubmit() {
+    // this.obj = {
+    //   city: this.city,
+    //   country: this.country,
+    //   winter: this.winter,
+    //   water: this.water,
+    //   culture: this.culture,
+    //   nature: this.nature,
+    //   entertainment: this.entertainment,
+    //   camping: this.culture,
+    //   cruise: this.cruise
+    // }
+    // console.log(this.obj)
 
-  // newCity = '';
+    this.City = this.city;
+    this.Country= this.country;
+    this.Winter= this.winter;
+    this.Water= this.water;
+    this.Culture= this.culture;
+    this.Nature= this.nature;
+    this.Entertainment= this.entertainment;
+    this.Camping= this.camping;
+    this.Cruise= this.cruise;
+  }
 
-  // data:any;
-  // servicedata:any;
-
-
-  // ngOnIt(){
-  //   this.dataarray.push(this.postLoc);
-  // }
-
-  // addForm(){
-  //   this.postLoc= new postLoc();
-  //   this.dataarray.push(this.postLoc);
-  // }
-
-  // onAddPost(postInput: HTMLTextAreaElement){
-  //   this.newCity ='';
-
-  // }
 }
